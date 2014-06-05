ec52559116d6d8a3075eb0c61513de4648aaccd5
kretzm <mike@michaelkretz.com>
2014-06-05 01:33:49 -0400

    Added some images/animations/UI

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ scotchApp.controller('mainController', function($scope) {
     // send in the input to the user to see if it matches
     $scope.custom = function(output) {
         $scope.output = deepaAnswer(output);
+        $scope.randBackground = randBackground();
     };
     $scope.reset = function()
     {
@@ -32,25 +33,45 @@ scotchApp.controller('mainController', function($scope) {
 // find matches based on user input
 function deepaAnswer(input) {
     var response = 'no match';
+    var randDefault = Math.floor((Math.random() * 4) + 1);
+    var exit = 0;
 
     // library of quotes
     var dictionary = {
         'quotes': {
             'home': 'Are you kidding me?',
-            'bathroom': 'Fine'
+            'bathroom': 'Fine.',
+            'have to': 'Child, stop your complaining. Yes.',
+            'problem': 'Do I really have to deal with this?',
+            'issue': "Talk to Ed."
         }       
     };
 
     // sort through the dictionary to find the match
     angular.forEach(dictionary.quotes, 
-        function(value, key) {
-
-        // match is found
-        if (input.indexOf(key) >= 0) {
+      function(value, key) {
+       
+        if (exit == 0) {
+          
+          // match is found
+          if (input.toLowerCase().indexOf(key) >= 0) {
             response = value;
-            return false;
-        }
-    });
+            exit = 1;
+          }
+          else 
+          {
+            if(randDefault == 1) {
+              response = "No. Just No.";
+            }
+            else if(randDefault == 2) {
+              response = "Yes. Seriously, Just Yes";
+            }
+            else {
+              response = "Go away. Stop bugging me.";
+            }     
+          }
+        }              
+      });
 
     // return the matched quote
     return response;
@@ -60,65 +81,23 @@ function deepaAnswer(input) {
 
 
 
+$(document).ready(function() {
 
+  //show hint text
+  $(".hint-text").on("click", function() {
+    $(".hint-content").fadeIn(600);
+  });
 
-
-
-
-
-
-//---------- Gradient Background ---------
-// http://codepen.io/zacharyhickson/pen/efaik
-
-$( document ).ready(function() {
-    var colors = new Array(
-      [230,105,147],
-      [58,164,178],
-      [40,26,88],
-      [232,24,122]);
-
-    var step = 0;
-    //color table indices for: 
-    // current color left
-    // next color left
-    // current color right
-    // next color right
-    var colorIndices = [0,1,2,3];
-
-    //transition speed
-    var gradientSpeed = 0.02;
-
-    function updateGradient() {
-      var c0_0 = colors[colorIndices[0]];
-      var c0_1 = colors[colorIndices[1]];
-      var c1_0 = colors[colorIndices[2]];
-      var c1_1 = colors[colorIndices[3]];
-
-      var istep = 1 - step;
-      var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-      var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-      var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-      var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
-
-      var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-      var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-      var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-      var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
-
-        $('#gradient').css({background: "-webkit-radial-gradient(80% 10%, circle, "+color1+", transparent), -webkit-radial-gradient(80% 50%, circle, "+color2+", transparent)"});
-        
-        step += gradientSpeed;
-        if ( step >= 1 )
-        {
-          step %= 1;
-          colorIndices[0] = colorIndices[1];
-          colorIndices[2] = colorIndices[3];
-          
-          //pick two new target color indices
-          //do not pick the same as the current one
-          colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-          colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-          
-        }
-    } setInterval(updateGradient,0.1);
+  randBackground();
 });
+
+
+function randBackground() {
+  var rand = Math.floor((Math.random() * 4) + 1);
+
+  //randomize the background
+  console.log($(".modal").length);
+  $(".response").css("background", "url(/images/deepa-bg-" + rand + ".jpg");
+  console.log(rand);
+}
+
